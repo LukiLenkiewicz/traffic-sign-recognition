8b55088a7eff9c308e9267fbf45e024fcecdedd4
Lukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2023-09-20 16:33:38 +0200

    model shapes fixed

--- src/model.py
+++ src/model.py
@@ -3,24 +3,26 @@ import torch.nn as nn
 
 class CNN(nn.Module):
     def __init__(self):
+        super().__init__()
         self.encoder = ConvBlock()
-        self.classifier = nn.Linear()
+        self.classifier = nn.Linear(56448, 32)
 
     def forward(self, x: torch.Tensor):
         x = self.encoder(x)
-        x = x.flatten()
+        x = x.reshape(x.shape[0], -1)
         x = self.classifier(x)
         return x
 
 
 class ConvBlock(nn.Module):
     def __init__(self):
+        super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32),
+            nn.Conv2d(3, 32, 3),
             nn.BatchNorm2d(32),
             nn.Dropout(p=0.3),
             nn.ReLU(),
-            nn.MaxPool2d()
+            nn.MaxPool2d(3)
         )
 
     def forward(self, x):
