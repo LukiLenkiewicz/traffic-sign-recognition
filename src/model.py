30d3112fb0f344f6b2b904d1af44379ed4ecc995
Lukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2023-09-24 22:52:43 +0200

    model checkpoint added, architecture modified

--- src/model.py
+++ src/model.py
@@ -4,8 +4,11 @@ import torch.nn as nn
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.encoder = ConvBlock()
-        self.classifier = nn.Linear(56448, 43)
+        self.encoder = nn.Sequential(
+            ConvBlock(input_channels=3, output_channels=32),
+            ConvBlock(input_channels=32, output_channels=64),
+        )
+        self.classifier = nn.Linear(10816, 43)
 
     def forward(self, x: torch.Tensor):
         x = self.encoder(x)
@@ -15,14 +18,14 @@ class CNN(nn.Module):
 
 
 class ConvBlock(nn.Module):
-    def __init__(self):
+    def __init__(self, input_channels=3, output_channels=32):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 32, 3),
-            nn.BatchNorm2d(32),
-            nn.Dropout(p=0.3),
+            nn.Conv2d(input_channels, output_channels, kernel_size=3, stride=1, padding=0),
+            nn.BatchNorm2d(output_channels),
+            # nn.Dropout(p=0.3),
             nn.ReLU(),
-            nn.MaxPool2d(3)
+            nn.MaxPool2d(kernel_size=3)
         )
 
     def forward(self, x):
